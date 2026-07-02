222def63f8c57d5f9b31beba3836da77c1dca61d
Gonza <juangonza@live.com.mx>
2026-07-02 15:58:10 -0600

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1933,4 +1933,4 @@ def get_informe_deudor(nombre):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
